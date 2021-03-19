c6139581ab72d386445fd2b56986442289385eb6
Morten Mortensen <morten@invisiblewrench.com>
2021-03-19 18:52:58 +0100

    fix device type

--- lib/midi_device.dart
+++ lib/midi_device.dart
@@ -1,12 +1,23 @@
+import 'package:flutter_midi_command_platform_interface/midi_port.dart';
+
 class MidiDevice {
-  String? name;
-  String? id;
-  String? type;
+  String name;
+  String id;
+  String type;
+  List<MidiPort> inputPorts = [];
+  List<MidiPort> outputPorts = [];
   bool connected;
 
   MidiDevice(this.id, this.name, this.type, this.connected);
 
-  Map<String, Object?> get toDictionary {
-    return {"name": name, "id": id, "type": type};
+  Map<String, Object> get toDictionary {
+    return {
+      "name": name,
+      "id": id,
+      "type": type,
+      "connected": connected,
+      // "inputs": inputPorts?.map((m) => m.toDictionary)?.toList(),
+      // "outputs": outputPorts?.map((m) => m.toDictionary)?.toList(),
+    };
   }
 }
